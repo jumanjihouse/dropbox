b2be88d9ba5baf588457e79f5b6e79cec1862d4b
Paul Morgan <jumanjiman@gmail.com>
2014-11-21 20:29:40 +0000

    resolve rubocop offenses

--- spec/dropbox/users_spec.rb
+++ spec/dropbox/users_spec.rb
@@ -9,7 +9,7 @@ describe 'users with interactive shells' do
   it 'should only include "root"' do
     # Which interactive shells are allowed in container?
     shells = docker_run('cat /etc/shells')
-    shells.map! { |s| s.chomp }.reject! { |s| s.match %r{/sbin/nologin} }
+    shells.map!(&:chomp).reject! { |s| s.match %r{/sbin/nologin} }
 
     # Which users have an interactive shell?
     users = []
